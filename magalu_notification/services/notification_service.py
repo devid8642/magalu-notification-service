6d19e5942ebc35048c3bf8d11ec83f6691d03c74
Dêvid Teófilo <nnick4673@gmail.com>
2024-07-07 15:10:08 -0300

    feat: notification service implemented

---
+++ magalu_notification/services/notification_service.py
@@ -0,0 +1,21 @@
+from magalu_notification.repositories.notification_repository import NotificationRepository, PostgresNotificationRepository
+from magalu_notification.models.notification import Notification
+from magalu_notification.db.postgres import PostgresConnection
+
+
+class NotificationService:
+    def __init__(self, notification_repository: NotificationRepository):
+        self.notification_repository = notification_repository
+    
+    async def create_notification(self, notification_data: dict) -> Notification:
+        notification = await self.notification_repository.create_notification(
+            Notification(**notification_data)
+        )
+
+        return notification
+
+
+def get_notification_service():
+    session = PostgresConnection.get_session()
+    notification_repository = PostgresNotificationRepository(session)
+    return NotificationService(notification_repository)
