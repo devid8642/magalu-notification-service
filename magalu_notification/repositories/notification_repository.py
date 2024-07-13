3018324ac76ce5c2e1e57a06e87e308c4dfe2dee
Dêvid Teófilo <nnick4673@gmail.com>
2024-07-13 18:33:56 -0300

    feat: get_notification() implemented in postgres notification repo

--- magalu_notification/repositories/notification_repository.py
+++ magalu_notification/repositories/notification_repository.py
@@ -3,19 +3,30 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 
 class NotificationRepository:
+    """Interface for NotificationRepository"""
     def __init__(self, session: AsyncSession):
         self.session = session
 
     async def create_notification(self, notification: Notification):
         raise NotImplementedError
+    
+    async def get_notification(self, notification_id: int) -> Notification | None:
+        raise NotImplementedError
 
 
 class PostgresNotificationRepository(NotificationRepository):
+    """Postgres implementation of NotificationRepository"""
     async def create_notification(self, notification: Notification) -> Notification:
         async with self.session() as session:
             async with session.begin():
                 session.add(notification)
                 await session.commit()
             await session.refresh(notification)
+
+        return notification
+    
+    async def get_notification(self, notification_id: int) -> Notification | None:
+        async with self.session() as session:
+            notification = await session.get(Notification, notification_id)
 
         return notification
