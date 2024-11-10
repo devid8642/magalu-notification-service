77e0b8bbb2b57ba243a3c33b3c6b8058022baeb1
Dêvid Teófilo <nnick4673@gmail.com>
2024-11-10 02:31:02 -0300

    test: tests for the cancel_notification controller

--- tests/test_notification_controllers.py
+++ tests/test_notification_controllers.py
@@ -1,5 +1,7 @@
 import pytest
 from unittest.mock import patch, AsyncMock
+from magalu_notification.schemas.general import SuccessSchema
+from magalu_notification.services.exceptions import NotificationNotFound, NotificationAlreadySended
 from magalu_notification.schemas.notification import SendNotificationSchema, NotificationSchema
 from fastapi.testclient import TestClient
 
@@ -41,3 +43,42 @@ def test_get_notification_not_found(client: TestClient):
 
         assert response.status_code == 404
         assert response.json() == {'message': 'Notificação não encontrada'}
+
+
+def test_cancel_notification_success(client: TestClient):
+    with patch('magalu_notification.main.get_notification_service') as mock_service:
+        mock_service_instance = mock_service.return_value
+        mock_service_instance.cancel_notification = AsyncMock(
+            return_value=None
+        )
+
+        response = client.post('/notification/cancel/1')
+
+        assert response.status_code == 200
+        assert response.json() == {'message': 'Notificação cancelada com sucesso'}
+
+
+def test_cancel_notification_not_found(client: TestClient):
+    with patch('magalu_notification.main.get_notification_service') as mock_service:
+        mock_service_instance = mock_service.return_value
+        mock_service_instance.cancel_notification = AsyncMock(
+            side_effect=NotificationNotFound()
+        )
+
+        response = client.post('/notification/cancel/999')
+
+        assert response.status_code == 404
+        assert response.json() == {'message': 'Notificação não encontrada'}
+
+
+def test_cancel_notification_already_sent(client: TestClient):
+    with patch('magalu_notification.main.get_notification_service') as mock_service:
+        mock_service_instance = mock_service.return_value
+        mock_service_instance.cancel_notification = AsyncMock(
+            side_effect=NotificationAlreadySended()
+        )
+
+        response = client.post('/notification/cancel/1')
+
+        assert response.status_code == 400
+        assert response.json() == {'message': 'Notificação já enviada'}
